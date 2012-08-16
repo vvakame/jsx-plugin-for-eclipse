356aeb7b8d92b83fb198af76867f4ade5e8497ab
vvakame <vvakame+dev@gmail.com>
2012-08-16 12:35:31 +0900

    add test sample.

---
+++ editor/src/test/java/net/vvakame/ide/jsx/editors/viewerconfiguration/JsxWordDetectorTest.java
@@ -0,0 +1,30 @@
+package net.vvakame.ide.jsx.editors.viewerconfiguration;
+
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.*;
+
+import org.eclipse.jface.text.Document;
+import org.eclipse.jface.text.rules.IRule;
+import org.eclipse.jface.text.rules.IToken;
+import org.eclipse.jface.text.rules.RuleBasedScanner;
+import org.eclipse.jface.text.rules.Token;
+import org.eclipse.jface.text.rules.WordRule;
+import org.junit.Test;
+
+public class JsxWordDetectorTest {
+
+	@Test
+	public void test() {
+		String document = "class";
+
+		WordRule rule = new WordRule(new JsxWordDetector());
+		rule.addWord("class", new Token(this));
+
+		RuleBasedScanner scanner = new RuleBasedScanner();
+		scanner.setRules(new IRule[] { rule });
+		scanner.setRange(new Document(document), 0, document.length());
+
+		IToken token = scanner.nextToken();
+		assertThat((JsxWordDetectorTest) token.getData(), is(this));
+	}
+}
