173ce2808bd1c04f73ac738ec60906fa2e874173
vvakame <vvakame+dev@gmail.com>
2012-08-30 17:01:14 +0900

    add testcase. try parse the JSX test code. JSX/t/run/*.jsx . now this test is ignored. because it's failure :(

--- jsx-antlr-syntax/src/test/java/net/vvakame/jsx/antlr/SyntaxTest.java
+++ jsx-antlr-syntax/src/test/java/net/vvakame/jsx/antlr/SyntaxTest.java
@@ -3,6 +3,10 @@ package net.vvakame.jsx.antlr;
 import static org.hamcrest.CoreMatchers.*;
 import static org.junit.Assert.*;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FilenameFilter;
 import java.io.IOException;
 import java.io.InputStream;
 
@@ -10,6 +14,7 @@ import net.vvakame.jsx.antlr.util.AntlrUtil;
 import net.vvakame.jsx.antlr.util.AntlrUtil.AntlrData;
 
 import org.antlr.runtime.RecognitionException;
+import org.junit.Ignore;
 import org.junit.Test;
 
 public class SyntaxTest {
@@ -42,8 +47,39 @@ public class SyntaxTest {
 		}
 	}
 
+	@Test
+	@Ignore
+	public void tryParseJsxTestCode() throws IOException, RecognitionException {
+		File gitRoot = getGitRootDirectory();
+
+		String[] jsxExistsDirPaths = { "JSX/t/run/" };
+
+		for (String dirPath : jsxExistsDirPaths) {
+			File dir = new File(gitRoot, dirPath);
+			File[] jsxFiles = dir.listFiles(new FilenameFilter() {
+				@Override
+				public boolean accept(File file, String name) {
+					return name.endsWith(".jsx");
+				}
+			});
+
+			for (File file : jsxFiles) {
+				InputStream stream = getStream(file);
+
+				String fileName = file.getAbsolutePath();
+				System.out.println(fileName);
+				assertParseSuccess(fileName, stream);
+			}
+		}
+	}
+
 	static InputStream getStream(String fileName) {
 		InputStream stream = SyntaxTest.class.getResourceAsStream(fileName);
+		return stream;
+	}
+
+	static InputStream getStream(File file) throws FileNotFoundException {
+		FileInputStream stream = new FileInputStream(file);
 		return stream;
 	}
 
@@ -59,5 +95,14 @@ public class SyntaxTest {
 		AntlrData result = AntlrUtil.parseStream(stream);
 		assertThat(fileName + " expected invalid syntax.",
 				result.parser.getNumberOfSyntaxErrors(), is(not(0)));
+	}
+
+	static File getGitRootDirectory() {
+		final String antlrProjectName = "jsx-antlr-syntax";
+		File gitRoot = new File("./").getAbsoluteFile();
+		while (!antlrProjectName.equals(gitRoot.getName())) {
+			gitRoot = gitRoot.getParentFile();
+		}
+		return gitRoot.getParentFile();
 	}
 }
