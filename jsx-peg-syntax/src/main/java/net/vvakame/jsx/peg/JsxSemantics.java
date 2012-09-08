00d838a88251c0cf85595241117734f5d64c7faf
vvakame <vvakame+dev@gmail.com>
2012-09-08 12:22:54 +0900

    add JsxSemantics#debug method.

--- jsx-peg-syntax/src/main/java/net/vvakame/jsx/peg/JsxSemantics.java
+++ jsx-peg-syntax/src/main/java/net/vvakame/jsx/peg/JsxSemantics.java
@@ -1,6 +1,7 @@
 package net.vvakame.jsx.peg;
 
 import mouse.runtime.Phrase;
+import mouse.runtime.PhraseExtend;
 import mouse.runtime.SemanticsBase;
 
 public class JsxSemantics extends SemanticsBase {
@@ -37,5 +38,38 @@ public class JsxSemantics extends SemanticsBase {
 			parent = new SyntaxTreeMouseImpl();
 		}
 		parent.children().add(tree);
+	}
+
+	public void variableDeclaration() {
+		dump();
+	}
+
+	public void dump() {
+		StringBuilder builder = new StringBuilder();
+
+		StackTraceElement stack = Thread.currentThread().getStackTrace()[2];
+		builder.append("at ").append(stack.getMethodName()).append("\t");
+		builder.append(stack.getFileName()).append("#")
+				.append(stack.getMethodName()).append(" L")
+				.append(stack.getLineNumber()).append("\n");
+
+		dumpPhrase(builder, "lhs", lhs());
+		for (int i = 0; i < rhsSize(); i++) {
+			Phrase rhs = rhs(i);
+			dumpPhrase(builder, "rhs(" + i + ")", rhs);
+		}
+		builder.append("\n");
+
+		System.out.println(builder.toString());
+	}
+
+	void dumpPhrase(StringBuilder builder, String prefix, Phrase phrase) {
+		final PhraseExtend extend = PhraseExtend
+				.get((mouse.runtime.ParserBase.Phrase) phrase);
+
+		builder.append("\t").append(prefix).append("\n");
+		builder.append("\t\trule=\"").append(extend.rule())
+				.append("\" : index=").append(extend.getStart()).append("\n");
+		builder.append("\t\ttext=\"").append(extend.text()).append("\"\n");
 	}
 }
