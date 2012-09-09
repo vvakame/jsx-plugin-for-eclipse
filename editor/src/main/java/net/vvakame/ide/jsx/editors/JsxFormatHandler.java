66a6dbaac80ccaec5b99759c9b8f24a30c24e6a0
u1aryz <u1aryz.d@gmail.com>
2012-09-09 17:12:31 +0900

    add format menu to context menu

---
+++ editor/src/main/java/net/vvakame/ide/jsx/editors/JsxFormatHandler.java
@@ -0,0 +1,37 @@
+package net.vvakame.ide.jsx.editors;
+
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.core.commands.IHandler;
+import org.eclipse.core.commands.IHandlerListener;
+
+public class JsxFormatHandler implements IHandler {
+
+	@Override
+	public void addHandlerListener(IHandlerListener handlerListener) {
+	}
+
+	@Override
+	public void dispose() {
+	}
+
+	@Override
+	public Object execute(ExecutionEvent event) throws ExecutionException {
+		return null;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		return true;
+	}
+
+	@Override
+	public boolean isHandled() {
+		return true;
+	}
+
+	@Override
+	public void removeHandlerListener(IHandlerListener handlerListener) {
+	}
+
+}
