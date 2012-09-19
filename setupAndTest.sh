b4e2fc445b4016782a036c81b3dfd6d3ab4c3f2a
vvakame <vvakame+dev@gmail.com>
2012-09-19 12:37:42 +0900

    update setupAndTest.sh

--- setupAndTest.sh
+++ setupAndTest.sh
@@ -1,9 +1,9 @@
-git submodule init
-git submodule update
-
-mvn -f jsx-peg-syntax/pom.xml clean test install eclipse:clean eclipse:eclipse -DdownloadSources=true
-mvn -f jsx-wrapper/pom.xml clean test install eclipse:clean eclipse:eclipse -DdownloadSources=true
-mvn -f editor/pom.xml clean test install eclipse:clean eclipse:eclipse -DdownloadSources=true
-mvn -f editor.feature/pom.xml clean test install eclipse:clean eclipse:eclipse -DdownloadSources=true
+git submodule init && \
+git submodule update && \
+\
+mvn -f jsx-peg-syntax/pom.xml clean test install eclipse:clean eclipse:eclipse -DdownloadSources=true && \
+mvn -f jsx-wrapper/pom.xml clean test install eclipse:clean eclipse:eclipse -DdownloadSources=true && \
+mvn -f editor/pom.xml clean test install eclipse:clean eclipse:eclipse -DdownloadSources=true && \
+mvn -f editor.feature/pom.xml clean test install eclipse:clean eclipse:eclipse -DdownloadSources=true && \
 mvn -f editor.test/pom.xml clean test install eclipse:clean eclipse:eclipse -DdownloadSources=true
 
