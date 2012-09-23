054931b00fd790a1caec442ed2010a53fb2e49e3
vvakame <vvakame+dev@gmail.com>
2012-09-23 18:44:07 +0900

    fix project settings. つーかーれーたー

--- setupAndTest.sh
+++ setupAndTest.sh
@@ -1,7 +1,6 @@
 git submodule init && \
 git submodule update && \
 \
-mvn clean test install eclipse:clean eclipse:eclipse -DdownloadSources=true && \
 mvn -f jsx-peg-syntax/pom.xml clean test install eclipse:clean eclipse:eclipse -DdownloadSources=true && \
 mvn -f jsx-wrapper/pom.xml clean test install eclipse:clean eclipse:eclipse -DdownloadSources=true && \
 mvn -f jsx.eclipse.plugin.core/pom.xml clean test install eclipse:clean eclipse:eclipse -DdownloadSources=true && \
