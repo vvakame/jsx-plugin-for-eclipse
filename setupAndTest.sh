0471763fb442383e98bcb31403419f7164ee368f
vvakame <vvakame+dev@gmail.com>
2012-09-23 18:12:28 +0900

    fix jsx.eclipse.plugin.core.tests/META-INF/MANIFEST.MF

--- setupAndTest.sh
+++ setupAndTest.sh
@@ -1,5 +1,11 @@
 git submodule init && \
 git submodule update && \
 \
-mvn clean test install eclipse:clean eclipse:eclipse -DdownloadSources=true
+mvn clean test install eclipse:clean eclipse:eclipse -DdownloadSources=true && \
+mvn -f jsx-peg-syntax/pom.xml clean test install eclipse:clean eclipse:eclipse -DdownloadSources=true && \
+mvn -f jsx-wrapper/pom.xml clean test install eclipse:clean eclipse:eclipse -DdownloadSources=true && \
+mvn -f jsx.eclipse.plugin.core/pom.xml clean test install eclipse:clean eclipse:eclipse -DdownloadSources=true && \
+mvn -f jsx.eclipse.plugin.core.tests/pom.xml clean test install eclipse:clean eclipse:eclipse -DdownloadSources=true && \
+mvn -f jsx.eclipse.plugin.feature/pom.xml clean test install eclipse:clean eclipse:eclipse -DdownloadSources=true && \
+mvn -f jsx.eclipse.plugin.updatesite/pom.xml clean test install eclipse:clean eclipse:eclipse -DdownloadSources=true
 
