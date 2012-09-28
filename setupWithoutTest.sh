c65b81d445db0e6e08709ad942f671dbdaa597cd
vvakame <vvakame+dev@gmail.com>
2012-09-28 14:51:42 +0900

    add setupWithoutTest.sh refs #12

---
+++ setupWithoutTest.sh
@@ -0,0 +1,10 @@
+git submodule init && \
+git submodule update && \
+\
+mvn -f jsx-peg-syntax/pom.xml clean test install eclipse:clean eclipse:eclipse -DdownloadSources=true -Dmaven.test.skip=true && \
+mvn -f jsx-wrapper/pom.xml clean test install eclipse:clean eclipse:eclipse -DdownloadSources=true -Dmaven.test.skip=true && \
+mvn -f jsx.eclipse.plugin.core/pom.xml clean test install eclipse:clean eclipse:eclipse -DdownloadSources=true -Dmaven.test.skip=true && \
+mvn -f jsx.eclipse.plugin.core.tests/pom.xml clean test install eclipse:clean eclipse:eclipse -DdownloadSources=true -Dmaven.test.skip=true && \
+mvn -f jsx.eclipse.plugin.feature/pom.xml clean test install eclipse:clean eclipse:eclipse -DdownloadSources=true -Dmaven.test.skip=true && \
+mvn -f jsx.eclipse.plugin.updatesite/pom.xml clean test install eclipse:clean eclipse:eclipse -DdownloadSources=true -Dmaven.test.skip=true
+
