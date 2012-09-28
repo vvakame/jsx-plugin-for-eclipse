f7a35be97f1704807bcfbf53772e8adb7fdd123f
vvakame <vvakame+dev@gmail.com>
2012-09-28 15:46:37 +0900

    update feature and updatesite settings. refs #12

--- setupWithoutTest.sh
+++ setupWithoutTest.sh
@@ -3,8 +3,8 @@ git submodule update && \
 \
 mvn -f jsx-peg-syntax/pom.xml clean test install eclipse:clean eclipse:eclipse -DdownloadSources=true -Dmaven.test.skip=true && \
 mvn -f jsx-wrapper/pom.xml clean test install eclipse:clean eclipse:eclipse -DdownloadSources=true -Dmaven.test.skip=true && \
-mvn -f jsx.eclipse.plugin.core/pom.xml clean test install eclipse:clean eclipse:eclipse -DdownloadSources=true -Dmaven.test.skip=true && \
-mvn -f jsx.eclipse.plugin.core.tests/pom.xml clean test install eclipse:clean eclipse:eclipse -DdownloadSources=true -Dmaven.test.skip=true && \
-mvn -f jsx.eclipse.plugin.feature/pom.xml clean test install eclipse:clean eclipse:eclipse -DdownloadSources=true -Dmaven.test.skip=true && \
-mvn -f jsx.eclipse.plugin.updatesite/pom.xml clean test install eclipse:clean eclipse:eclipse -DdownloadSources=true -Dmaven.test.skip=true
+mvn -f net.vvakame.jsx.eclipse.plugin.core/pom.xml clean test install eclipse:clean eclipse:eclipse -DdownloadSources=true -Dmaven.test.skip=true && \
+mvn -f net.vvakame.jsx.eclipse.plugin.core.tests/pom.xml clean test install eclipse:clean eclipse:eclipse -DdownloadSources=true -Dmaven.test.skip=true && \
+mvn -f net.vvakame.jsx.eclipse.feature/pom.xml clean test install eclipse:clean eclipse:eclipse -DdownloadSources=true -Dmaven.test.skip=true && \
+mvn -f net.vvakame.jsx.eclipse.updatesite/pom.xml clean test install eclipse:clean eclipse:eclipse -DdownloadSources=true -Dmaven.test.skip=true
 
